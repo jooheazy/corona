87b46f83e995d29aaf89b12c51213f13cd89fd7b
Jonathan Jesus <jonathan.jesus@sinqia.com.br>
2020-12-23 10:50:32 -0300

    Subida do Corona

---
+++ corona/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace corona
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBusca_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            List<string> lstExt = txtbusca.Text.Split(',').ToList();
+            DirectoryInfo dirBusca = new DirectoryInfo(txtcaminho.Text);
+
+            string lstarq = "";
+
+            foreach (string v in lstExt)
+            {
+                foreach (FileInfo f in dirBusca.GetFiles(v, SearchOption.AllDirectories))
+                {
+                    lstarq += f.FullName + "\r\n";
+                }
+            }
+
+            File.WriteAllText("D:\\Temp\\corona.txt", lstarq);
+        }
+    }
+}
